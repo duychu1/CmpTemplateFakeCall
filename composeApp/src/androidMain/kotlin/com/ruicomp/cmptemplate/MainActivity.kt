cfad5a17ce32b3450d4109f511fc64327b6abe10
DuyChu <chuduy3121@gmail.com>
2025-06-15 14:13:45 +0700

    Update dard icon status bar

--- composeApp/src/androidMain/kotlin/com/ruicomp/cmptemplate/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/ruicomp/cmptemplate/MainActivity.kt
@@ -4,8 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
 import com.ruicomp.cmptemplate.app.App
 
 class MainActivity : ComponentActivity() {
@@ -14,7 +18,23 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            App()
+//            val isDarkTheme = isSystemInDarkTheme()
+            val isDarkTheme = false
+
+            // Key logic: We want dark icons when the theme is NOT dark
+            val useDarkIcons = !isDarkTheme
+
+            // Use DisposableEffect to set the status bar icons
+            // It will be cleaned up when the composable leaves the composition
+            DisposableEffect(useDarkIcons) {
+                val window = (this@MainActivity).window
+                val insetsController = WindowCompat.getInsetsController(window, window.decorView)
+                insetsController.isAppearanceLightStatusBars = useDarkIcons
+                onDispose {} // No cleanup needed
+            }
+            MaterialTheme {
+                App()
+            }
         }
     }
 }
