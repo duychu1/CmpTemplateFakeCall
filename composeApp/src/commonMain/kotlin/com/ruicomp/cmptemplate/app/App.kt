af0535a88daeca9b7537051163f5971ad886ee6b
DuyChu <chuduy3121@gmail.com>
2025-06-17 18:11:02 +0700

    Update padding system navigation bar

--- composeApp/src/commonMain/kotlin/com/ruicomp/cmptemplate/app/App.kt
+++ composeApp/src/commonMain/kotlin/com/ruicomp/cmptemplate/app/App.kt
@@ -1,7 +1,13 @@
 package com.ruicomp.cmptemplate.app
 
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.navigationBars
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
 import com.ruicomp.cmptemplate.navigation.NavGraph
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -9,6 +15,10 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 @Preview
 fun App() {
     MaterialTheme {
-        NavGraph()
+        Surface(modifier = Modifier.fillMaxSize()
+            .windowInsetsPadding(WindowInsets.navigationBars)
+        ) {
+            NavGraph()
+        }
     }
 } 
